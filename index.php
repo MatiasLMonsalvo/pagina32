c9f7a4d44261ae2c8f352a1cbaf0fed1a59c29ec
Alejandro Romero <romero.m.alejandro@gmail.com>
2018-07-26 20:17:59 +0000

    Archivos de prueba para mati

--- index.php
+++ index.php
@@ -2,8 +2,20 @@
 <html lang="es" dir="ltr">
 <head>
 	<title>Pagina de Mati</title>
+	<style type="text/css">
+		.mati {
+			border: 1px dashed #A00;
+			padding:1%;
+		}
+	</style>
 </head>
 <body>
-	Hola Mati
+	<div style="text-align:center"><h1>Hola Mati</h1></div>
+	<div class="mati pepe">
+		asdasd
+		<br>
+		<a href="otro.html">Ir a otro archivo</a>
+	</div>
+	
 </body>
 </html>
